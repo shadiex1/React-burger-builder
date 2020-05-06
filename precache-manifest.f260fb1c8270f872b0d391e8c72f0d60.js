b87cfab0ae28bef1db81d05946c8009ebe6214e1
shady ahmed <shadiex@hotmail.com>
2020-05-06 04:46:51 +0200

    Updates

---
+++ precache-manifest.f260fb1c8270f872b0d391e8c72f0d60.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9b1e09e7aa0ae01007d168bfddd11d0b",
+    "url": "/React-burger-builder/index.html"
+  },
+  {
+    "revision": "c06b86a2b8f97bfe8713",
+    "url": "/React-burger-builder/static/css/main.e27647af.chunk.css"
+  },
+  {
+    "revision": "7ccd18b497b00bb9388c",
+    "url": "/React-burger-builder/static/js/2.ee896267.chunk.js"
+  },
+  {
+    "revision": "928d7b5eb39d16fe9a880722c974b51e",
+    "url": "/React-burger-builder/static/js/2.ee896267.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c06b86a2b8f97bfe8713",
+    "url": "/React-burger-builder/static/js/main.236b0bfc.chunk.js"
+  },
+  {
+    "revision": "8efe561c1a5cc508e822",
+    "url": "/React-burger-builder/static/js/runtime-main.146915b9.js"
+  },
+  {
+    "revision": "b8503d262bffbfb7c67fd6762963e7d1",
+    "url": "/React-burger-builder/static/media/burger-logo.b8503d26.png"
+  }
+]);
